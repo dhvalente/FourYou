00725b3af8ea77a801abec15e810a798dee0cfe5
rafael-oliveira-hellz <95747012+rafael-oliveira-hellz@users.noreply.github.com>
2022-06-07 13:07:27 -0300

    Feature account2 (#3)
    * Create Account Model
    
    * Update account model
    
    * Update account model
    
    * Create Account-related Models
    
    Co-authored-by: Vinicius Jesus <viniciusantos2105@gmail.com>
    Co-authored-by: José Robson <joserobsonsiqueira23@hotmail.com>

---
+++ src/main/java/br/com/foursys/fourcamp/fourbank/model/CheckingAccount.java
@@ -0,0 +1,24 @@
+package br.com.foursys.fourcamp.fourbank.model;
+
+import javax.persistence.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CheckingAccount extends Account{
+
+	private Double maintenanceFee;
+	
+	public Double discountFee(Double balance, Double maintenanceFee) {
+		Double fee;
+		this.balance -= maintenanceFee;
+		return this.balance;
+	}
+}
