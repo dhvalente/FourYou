ea0cd06a2eac91e9094c86b94078bc7ba23aa660
Thales Borba <tborba@server01.foursys.local>
2022-06-07 10:34:44 -0300

    Pix model and Enum done

---
+++ src/main/java/br/com/foursys/fourcamp/fourbank/enums/PixKeyType.java
@@ -0,0 +1,21 @@
+package br.com.foursys.fourcamp.fourbank.enums;
+
+import lombok.*;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public enum PixKeyType {
+
+    CPF(0),
+    EMAIL(1),
+    PHONE(2),
+    RANDOM(3);
+
+    private Integer memberName;
+
+    private Integer memberName() {
+        return this.memberName;
+    }
+
+}
