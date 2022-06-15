0b1fe86e5e8739204939c509f5f073a76dd7bcd6
Rafael de Oliveira <95747012+rafael-oliveira-hellz@users.noreply.github.com>
2022-06-15 10:12:34 -0300

    Feature card (#8)
    * create class card
    
    * Create Card, Model, Controller, Service
    
    Co-authored-by: Diogo Henrique Valente <dvalente@server01.foursys.local>
    Co-authored-by: Diogo Valente <diogovalente@hotmail.com.br>

---
+++ src/main/java/br/com/foursys/fourcamp/fourbank/model/CreditCard.java
@@ -0,0 +1,27 @@
+package br.com.foursys.fourcamp.fourbank.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tb_credit_card")
+public class CreditCard extends Card{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Double creditLimit;	
+
+	
+}
