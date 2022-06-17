628f08481bfa71c14c50106594307d2cfdc546e2
Thales Borba <tborba@server01.foursys.local>
2022-06-17 12:02:17 -0300

    Added CreditCardInstallments business rules

--- src/main/java/br/com/foursys/fourcamp/fourbank/model/CreditCard.java
+++ src/main/java/br/com/foursys/fourcamp/fourbank/model/CreditCard.java
@@ -1,15 +1,14 @@
 package br.com.foursys.fourcamp.fourbank.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Inheritance;
 import javax.persistence.Table;
-
-import br.com.foursys.fourcamp.fourbank.util.GenerateCardNumber;
-import br.com.foursys.fourcamp.fourbank.util.GenerateCardNumber.Label;
-
 import java.io.Serializable;
+import java.util.List;
 
 @Entity
 @Data
@@ -20,6 +19,8 @@ import java.io.Serializable;
 public class CreditCard extends Card implements Serializable{
 	private static final long serialVersionUID = 1L;
 
-	private Double creditLimit;	
+	private Double creditLimit;
+
+	private List<CreditCardInstallment> creditCardInstallments;
 
 }
--- src/main/java/br/com/foursys/fourcamp/fourbank/service/PaymentMethodService.java
+++ src/main/java/br/com/foursys/fourcamp/fourbank/service/PaymentMethodService.java
@@ -6,14 +6,18 @@ import br.com.foursys.fourcamp.fourbank.exceptions.AccountNotFoundException;
 import br.com.foursys.fourcamp.fourbank.exceptions.InvalidParametersException;
 import br.com.foursys.fourcamp.fourbank.exceptions.PaymentNotFoundException;
 import br.com.foursys.fourcamp.fourbank.exceptions.UnregisteredPaymentMethodException;
+import br.com.foursys.fourcamp.fourbank.model.CreditCard;
+import br.com.foursys.fourcamp.fourbank.model.CreditCardInstallment;
 import br.com.foursys.fourcamp.fourbank.model.PaymentMethod;
 import br.com.foursys.fourcamp.fourbank.repository.CheckingAccountRepository;
+import br.com.foursys.fourcamp.fourbank.repository.CreditCardRepository;
 import br.com.foursys.fourcamp.fourbank.repository.PaymentMethodRepository;
 import br.com.foursys.fourcamp.fourbank.repository.SavingsAccountRepository;
 import br.com.foursys.fourcamp.fourbank.util.PaymentMethodValidations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,6 +29,8 @@ public class PaymentMethodService {
     private SavingsAccountRepository savingsAccountRepository;
     @Autowired
     private CheckingAccountRepository checkingAccountRepository;
+    @Autowired
+    private CreditCardRepository creditCardRepository;
 
     @Autowired
     public PaymentMethodService(PaymentMethodRepository paymentMethodRepository) {
@@ -66,21 +72,44 @@ public class PaymentMethodService {
 
     private PaymentMethod paymentMethodIsValid(PaymentMethod paymentMethod) throws InvalidParametersException,
             UnregisteredPaymentMethodException, AccountNotFoundException {
-        if (!PaymentMethodValidations.paymentMethodValidation(paymentMethod.getType().getType(),
-                paymentMethod.getIdentifier())) {
-            throw new InvalidParametersException();
+        validatePaymentMethod(paymentMethod);
+        checkIfAccountHasMethod(paymentMethod);
+        checkIfAccountIsRegistered(paymentMethod);
+        UpdateCreditCardInstallments(paymentMethod);
+        //checar limite do cartão de débito
+        //Alterar limite do cartão de crédito, ou o saldo da conta
+        return paymentMethod;
+    }
+
+    private void UpdateCreditCardInstallments(PaymentMethod paymentMethod) {
+        if (paymentMethod.getType().equals(0)) {
+            //Diogo irá criar o findByCardNumber
+            CreditCard creditCard = creditCardRepository.findByCardNumber(paymentMethod.getIdentifier());
+            CreditCardInstallment installment = new CreditCardInstallment(LocalDate.now(),
+                    paymentMethod.getNumberOfInstallments());
+            creditCard.getCreditCardInstallments().add(installment);
         }
-        if (!PaymentMethodValidations.checkPaymentBoundsToAccount(paymentMethod.getOriginAccount(),
-                paymentMethod.getType().getType(), paymentMethod.getIdentifier())){
-            throw new UnregisteredPaymentMethodException();
-        }
+    }
+
+    private void checkIfAccountIsRegistered(PaymentMethod paymentMethod) throws AccountNotFoundException {
         if (savingsAccountRepository.findById(paymentMethod.getOriginAccount().getId()).isEmpty() ||
                 checkingAccountRepository.findById(paymentMethod.getOriginAccount().getId()).isEmpty() ) {
             throw new AccountNotFoundException(paymentMethod.getOriginAccount().getId());
         }
-        //checar limites
-        //Alterar limite do cartão de crédito, ou o saldo da conta
-        return paymentMethod;
+    }
+
+    private void checkIfAccountHasMethod(PaymentMethod paymentMethod) throws UnregisteredPaymentMethodException {
+        if (!PaymentMethodValidations.checkPaymentBoundsToAccount(paymentMethod.getOriginAccount(),
+                paymentMethod.getType().getType(), paymentMethod.getIdentifier())){
+            throw new UnregisteredPaymentMethodException();
+        }
+    }
+
+    private void validatePaymentMethod(PaymentMethod paymentMethod) throws InvalidParametersException {
+        if (!PaymentMethodValidations.paymentMethodValidation(paymentMethod.getType().getType(),
+                paymentMethod.getIdentifier())) {
+            throw new InvalidParametersException();
+        }
     }
 
     public PaymentMethod findById(Long id) throws PaymentNotFoundException {
